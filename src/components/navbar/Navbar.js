7ce3b885e3d1b55e76389422bfc4cf3638c51049
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-04-01 20:25:39 +0800

    minor changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
+import React, { useState } from 'react'
 import styles from './Navbar.module.scss'
 
 import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
 
 import Logo from 'assets/img/logo.png'
 
@@ -28,21 +30,50 @@ const menu = [
 ]
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  function toggleMobileMenu() {
+    setIsMobileMenuOpen(m => !m)
+  }
   return (
-    <div className={styles.navbar}>
-      <div className={styles.inner}>
-        <Link to="/"><img src={Logo} alt="Logo"/></Link>
-        <div className={styles.menu}>
-          {menu.map(m => (
-            <Link to={m.link} key={m.link}><div>{m.text}</div></Link>
-          ))}
-        </div>
-        <div className={styles.buttons}>
-          <Link to='/login'><button className={styles.signIn}>Sign in</button></Link>
-          <Link to='/login'><button className={styles.getStarted}>Get started</button></Link>
+    <React.Fragment>
+      <div className={styles.navbar}>
+        <div className={styles.inner}>
+          <Link to="/"><img src={Logo} alt="Logo"/></Link>
+          <div className={styles.menu}>
+            {menu.map(m => (
+              <Link to={m.link} key={m.link}><div>{m.text}</div></Link>
+            ))}
+          </div>
+          <div className={styles.buttons}>
+            <Link to='/login'><button className={styles.signIn}>Sign in</button></Link>
+            <Link to='/login'><button className={styles.getStarted}>Get started</button></Link>
+            <div className={styles.mobileMenuButton}>
+              <div className={isMobileMenuOpen ? styles.closeButton : styles.openButton} onClick={toggleMobileMenu}>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <CSSTransition
+        in={isMobileMenuOpen}
+        timeout={300}
+        classNames="mobile-menu"
+        unmountOnExit
+      >
+        <div className={styles.mobileMenu}>
+          <div className={styles.menuList}>
+            {menu.map(m => (
+              <Link to={m.link} key={m.link} onClick={toggleMobileMenu}><div>{m.text}</div></Link>
+            ))}
+          </div>
+          <div className={styles.buttonList}>
+            <button className={styles.getStarted} onClick={toggleMobileMenu}>Get started</button>
+            <button className={styles.signIn} onClick={toggleMobileMenu}>Sign in</button>
+          </div>
+        </div>
+      </CSSTransition>
+    </React.Fragment>
   )
 }
 
