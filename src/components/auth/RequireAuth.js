66c0356dfdaa833bf2b7984a5dfe9d71f8317ec8
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-04-01 15:47:57 +0800

    Fix hard-coded [object Object] string (thanks!)

--- src/components/auth/RequireAuth.js
+++ src/components/auth/RequireAuth.js
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import { useLocation, Navigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import LoginPage from '../../modules/login-page/LoginPage'
 
 function RequireAuth({ children }) {
-  const location = useLocation();
-
   const [isAuthenticated] = useState(false);
 
   if (!isAuthenticated) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <React.Fragment>
+      <p style={{ margin: '10px 20px' }}>Please login (or create account) to access this feature</p>
+      <LoginPage />
+    </React.Fragment>
   }
 
   return children;
