a638bc2a036e4709968a35e15bb692322e267f70
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-05-03 04:33:22 +0800

    I should get a raise for this.

--- src/modules/landing-page/footer/Footer.js
+++ src/modules/landing-page/footer/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
   return (
     <div className={styles.footer}>
       <p>Copyright <a href="https://tuhuynh.com" target="_blank" rel="noreferrer">&copy; 2022 Tu Huynh</a></p>
-      <p>UI used from <a href="https://coinbase.com" target="_blank" rel="noreferrer">Coinbase Inc &trade;</a></p>
     </div>
   )
 }
